f914d7948d40b2a01085a7a00b74421e8e6146de
xmnboy <xmnboy@yahoo.com>
2014-08-21 21:30:26 -0700

    split app init into debug and events initialization functions on app.Ready event

--- www/js/init-app.js
+++ www/js/init-app.js
@@ -13,9 +13,9 @@
 
 window.app = window.app || {} ;         // there should only be one of these, but...
 
-app.initApplication = function() {
+app.initAppEvents = function() {
     "use strict" ;
-    var fName = "app.initApplication():" ;
+    var fName = "app.initAppEvents():" ;
     console.log(fName, "entry") ;
 
 // Main app init point (where we capture app.Ready event).
@@ -39,36 +39,25 @@ app.initApplication = function() {
     // Initialize app event handlers.
     // See app.js for example of the event handler being attached.
 
-    // ...overly simple example...
-    // var el = document.getElementById("id_btnHello") ;
-    // el.addEventListener("click", myEventHandler, false) ;
+// ...overly simple example...
+//    var el, evt ;
+//
+//    if( navigator.msPointerEnabled )                            // if on a Windows 8 machine
+//        evt = "click" ;                                         // let touch become a click event
+//    else                                                        // else, assume touch events available
+//        evt = "touchend" ;                                      // not optimum, but works
+//
+//    el = document.getElementById("id_btnHello") ;
+//    el.addEventListener(evt, myEventHandler, false) ;
 
     // after init is all done is a good time to remove our splash screen
-
-    // see https://github.com/01org/appframework/blob/master/documentation/detail/%24.ui.launch.md
-    // Do following if you disabled App Framework autolaunch (in index.html, for example)
-    // $.ui.launch() ;
 
     app.hideSplashScreen() ;                // a splash screen is optional for your app
 
     // ...and whatever else you want to do now that the app has started...
     // The following is just for debug, not required; keep it if you want or get rid of it.
 
-    if( window.device && device.cordova ) {                         // old Cordova 2.x version detection
-        console.log("device.version: " + device.cordova) ;          // print the cordova version string...
-        console.log("device.model: " + device.model) ;
-        console.log("device.platform: " + device.platform) ;
-        console.log("device.version: " + device.version) ;
-    }
-
-    if( window.cordova && cordova.version ) {                       // only works in Cordova 3.x
-        console.log("cordova.version: " + cordova.version) ;        // print new Cordova 3.x version string...
-
-        if( cordova.require ) {                                     // print included cordova plugins
-            console.log(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
-        }
-    }
-
+    // ...other miscellaneous init stuff here...
 
     // app initialization is done
     // app event handlers are ready
@@ -76,7 +65,37 @@ app.initApplication = function() {
 
     console.log(fName, "exit") ;
 } ;
-document.addEventListener("app.Ready", app.initApplication, false) ;
+document.addEventListener("app.Ready", app.initAppEvents, false) ;
+
+
+
+app.initAppDebug = function() {
+    "use strict" ;
+    var fName = "app.initAppDebug():" ;
+    console.log(fName, "entry") ;
+
+// Just a bunch of useful debug console.log() messages.
+// Runs after underlying device native code and webview/browser is ready.
+// The following is just for debug, not required; keep it if you want or get rid of it.
+
+    if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
+        console.log("device.version: " + device.cordova) ;      // print the cordova version string...
+        console.log("device.model: " + device.model) ;
+        console.log("device.platform: " + device.platform) ;
+        console.log("device.version: " + device.version) ;
+    }
+
+    if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
+        console.log("cordova.version: " + cordova.version) ;    // print new Cordova 3.x version string...
+
+        if( cordova.require ) {                                 // print included cordova plugins
+            console.log(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
+        }
+    }
+
+    console.log(fName, "exit") ;
+} ;
+document.addEventListener("app.Ready", app.initAppDebug, false) ;
 
 
 
